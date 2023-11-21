7689b85c2d8241ced317bfc97efa35f7fc604c53
Breno E S L Reis <brereis@outlook.com>
2023-11-21 18:12:55 -0300

    SIstema Inimigo Com lista sem destruicão

--- Assets/Script/InimigoControl.cs
+++ Assets/Script/InimigoControl.cs
@@ -7,16 +7,37 @@ using UnityEngine.Pool;
 public class InimigoControl : MonoBehaviour
 {
     public Transform _alvoInicial;
+    public Transform _grupoInimigos;
+    
+    public List<GameObject> _inimigoList = new List<GameObject>();
+    public List<GameObject> _inimigoListas = new List<GameObject>();
+    
+    [SerializeField] float _timeIni, _contador;
+    bool _timeAtivo;
+
+    public List<Transform> _pos1List = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Inimigo_1ON", 1);
+        // nao necessario --> Invoke("Inimigo_1ON", 1);
+        _timeIni = 3;
+        _contador = _timeIni;
+        _timeAtivo = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_timeAtivo)
+        {
+            _contador -= Time.deltaTime;
+            if (_contador < 0)
+            {
+                // _timeAtivo = false;
+                _contador = _timeIni;
+                Inimigo_1ON();
+            }
+        }
     }
 
     void Inimigo_1ON()
@@ -24,7 +45,10 @@ public class InimigoControl : MonoBehaviour
         GameObject bullet = InimigoTipo1._SharedInstance.GetPooledObject();
         if (bullet != null)
         {
-            // bullet.transform.position = turret.transform.position;
+            int number = Random.Range(0, _pos1List.Count);
+             bullet.transform.position = _pos1List[number].transform.position;
+            //bullet.transform.SetParent(gameObject.transform);
+            bullet.transform.SetParent(_grupoInimigos.transform);
             bullet.GetComponent<Movimento_inimigo>()._player = _alvoInicial;
             bullet.SetActive(true);
         }
--- Assets/Script/InimigoTipo1.cs
+++ Assets/Script/InimigoTipo1.cs
@@ -9,6 +9,8 @@ public class InimigoTipo1 : MonoBehaviour
     public List<GameObject> _pooledObjects;
     public GameObject _objectToPool;
     public int _amountToPool;
+
+    InimigoControl _inicontrol;
     void Awake()
     {
         _SharedInstance = this;
@@ -17,12 +19,15 @@ public class InimigoTipo1 : MonoBehaviour
     void Start()
     {
         _pooledObjects = new List<GameObject>();
+        _inicontrol = Camera.main.GetComponent<InimigoControl>();
         GameObject tmp;
         for (int i = 0; i < _amountToPool; i++)
         {
             tmp = Instantiate(_objectToPool);
             tmp.SetActive(false);
             _pooledObjects.Add(tmp);
+            _inicontrol._inimigoList.Add(tmp);
+            _inicontrol._inimigoListas.Add(tmp);
         }
 
     }
