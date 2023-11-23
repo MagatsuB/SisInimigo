038b6a33f7f1c2596371593c9cb1007f8f33e64b
Breno E S L Reis <brereis@outlook.com>
2023-11-23 13:35:06 -0300

    Inimigo ressucitando com bug
    o efeito de particula de restart esta como efeito de respawn

--- Assets/Script/HitInimigo.cs
+++ Assets/Script/HitInimigo.cs
@@ -5,31 +5,51 @@ using UnityEngine;
 public class HitInimigo : MonoBehaviour
 {
     Movimento_inimigo _moveInim;
-    public GameObject _particDead, _particRestart; 
+    public GameObject _particDead, _particRestart;
+    public MeshRenderer _meshren;
+    public Collider _collider;
     private void Start()
     {
         _moveInim = GetComponent<Movimento_inimigo>();
+        _meshren = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hit");
+        //Debug.Log("Hit");
         if (other.gameObject.CompareTag("PlayerAttack"))
         {
-
             StartCoroutine(Morte());
+            
         }
     }
 
     IEnumerator Morte()
     {
+        
         _moveInim._checkMove = false;
-        _particDead.SetActive(true);
+        _particRestart.SetActive(true);
+        _meshren.enabled = false;
         yield return new WaitForSeconds(1);
+        _collider.enabled = false;
+        _meshren.enabled = true;
+        //_particDead.SetActive(false);
         gameObject.SetActive(false);
     }
 
-    private void Reset()
+    IEnumerator RestartTime()
     {
-        
+        _particRestart.SetActive(true);
+       /* yield return new WaitForSeconds(1);
+        _meshren.enabled = true;
+        _collider.enabled = true;*/
+        yield return new WaitForSeconds(0.5f);
+        _moveInim._checkMove = true;
     }
+
+    public void Restart()
+    {
+        StartCoroutine(RestartTime());
+    }
+
 }
--- Assets/Script/InimigoControl.cs
+++ Assets/Script/InimigoControl.cs
@@ -46,11 +46,12 @@ public class InimigoControl : MonoBehaviour
         if (bullet != null)
         {
             int number = Random.Range(0, _pos1List.Count);
-             bullet.transform.position = _pos1List[number].transform.position;
+            bullet.transform.position = _pos1List[number].transform.position;
             //bullet.transform.SetParent(gameObject.transform);
             bullet.transform.SetParent(_grupoInimigos.transform);
             bullet.GetComponent<Movimento_inimigo>()._player = _alvoInicial;
             bullet.SetActive(true);
+            bullet.GetComponent<HitInimigo>().Restart();
         }
     }
 }
